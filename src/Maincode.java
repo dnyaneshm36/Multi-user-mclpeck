7d2508e5a077940e323d244455d73625a91933d4
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-05-30 16:54:16 +0530

    m clpkes is added .

--- src/Maincode.java
+++ src/Maincode.java
@@ -7,6 +7,8 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.w3c.dom.css.ElementCSSInlineStyle;
+
 import it.unisa.dia.gas.jpbc.Element;
 import it.unisa.dia.gas.jpbc.Pairing;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
@@ -209,6 +211,77 @@ public class Maincode {
             SecreteKey temp = new SecreteKey(Yi, Ti);
             receiversSecreteKeys.add(temp);
 
+        }
+
+
+        //mCLPECK
+        int n = 10;
+        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<Element> Bjs = new ArrayList<Element>();
+        String word= "first";
+        words.add(word);
+        word = "second";
+        words.add(word);
+        word = "third";
+        words.add(word);
+        word = "four";
+        words.add(word);
+        word = "five";
+        words.add(word);
+        word = "six";
+        words.add(word);
+        word = "seven";
+        words.add(word);
+        word = "eight";
+        words.add(word);
+        word = "nine";
+        words.add(word);
+        word = "ten";
+        words.add(word);
+
+
+        Element r = pairing.getZr().newRandomElement(); 
+        Element rdash = pairing.getZr().newRandomElement();
+
+        Element A = g.duplicate();
+        A.powZn(r);
+
+        for (int i = 0 ;i< m ;i++)
+        {
+            String ids = receiverId.get(i);
+            Element Yj = receiversPublicKeys.get(i).getYi();
+            Element Tj = receiversPublicKeys.get(i).getTi();
+            
+            Element betaj = hash3(ids, pairing, gpub, Yj, Tj);
+            Element alphai = receiversPartialPrivatekeys.get(i).getAlphai();
+
+            Element temp = Yj.duplicate();
+            temp.powZn(betaj);
+            temp.mul(Tj);
+
+            Element tem = gpub.duplicate();
+            tem.powZn(alphai);
+            temp.mul(tem);
+
+            temp.powZn(rdash);
+
+            Element Bj = temp.duplicate();
+            Bjs.add(Bj);
+        }
+
+        ArrayList<Element> Cjs = new ArrayList<Element>();
+
+        for ( int  i = 0 ;i< n ;i++)
+        {
+            
+            Element Hi = hash1(words.get(i), pairing);
+            Element Fi = hash2(words.get(i),pairing);
+
+            Hi.powZn(r);
+            Fi.powZn(rdash);
+            Element Ci = Hi.duplicate();
+            Ci.mul(Fi);
+            Cjs.add(Ci);
         }
     }
 }
